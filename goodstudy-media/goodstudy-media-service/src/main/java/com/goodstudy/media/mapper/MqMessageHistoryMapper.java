0332b5347fd6feb71db202de2625d5a454566df2
ZHENG ZHIJUN <1848892406@qq.com>
2023-04-03 21:22:33 +0800

    media 环境搭建

---
+++ goodstudy-media/goodstudy-media-service/src/main/java/com/goodstudy/media/mapper/MqMessageHistoryMapper.java
@@ -0,0 +1,15 @@
+package com.goodstudy.media.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.goodstudy.media.model.po.MqMessageHistory;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface MqMessageHistoryMapper extends BaseMapper<MqMessageHistory> {
+
+}
---
+++ goodstudy-media/goodstudy-media-service/src/main/java/com/goodstudy/media/mapper/MqMessageMapper.java
@@ -0,0 +1,15 @@
+package com.goodstudy.media.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.goodstudy.media.model.po.MqMessage;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface MqMessageMapper extends BaseMapper<MqMessage> {
+
+}
